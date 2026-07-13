8b4fd07a6d3914146e42dc2fb91642e1604b6f65
jire <jireta@gmail.com>
2026-07-13 08:24:26 +0800

    fix flutter vercel build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+export PATH="$PATH:$PWD/flutter/bin"
+
+flutter config --enable-web
+
+flutter pub get
+
+flutter build web --release
